519f8a6da19a0a26f1521b9a1c9d56a86239a912
Artem Tkachuk <arti17900@gmail.com>
2019-09-19 20:36:43 +0600

    Add product model

--- eshop/webapp/admin.py
+++ eshop/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'count', 'price']
+    list_filter = ['category']
+    search_fields = ['name', 'category']
+    fields = ['name', 'category', 'count', 'price']
+    list_display_links = ['id', 'name', 'category']
+
+
+admin.site.register(Product, ProductAdmin)
--- eshop/webapp/models.py
+++ eshop/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+category_choices = [('other', 'Other'), ('notebooks', 'Notebooks'), ('computers', 'Computers'), ('monitors', 'Monitors'), ('printers', 'Printers')]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, verbose_name='Описание')
+    category = models.CharField(max_length=30, null=False, choices=category_choices, default='other', verbose_name='Категория')
+    count = models.IntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return f'{self.name} - {self.category} - {self.price}'
